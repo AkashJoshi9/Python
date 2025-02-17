77008904c5a9164e3b4966e78d31f14789ce594c
Akash Joshi <117718217+AkashJoshi9@users.noreply.github.com>
2025-02-18 00:00:05 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,20 @@
+# assignment-1
+
+#1 Print a following text as it appears:
+'''       python is fun.
+          "Quotes" and 'single quotes' can be tricky."       '''
+#Ans
+print('''"Python is fun.\n"Quotes" and 'single quotes' can be tricky.''') 
+# OR
+print("Python is fun.")
+print("\"Quotes\" and 'single quotes' can be tricky.") 
+
+#2 For a business create 3 variables to store - name, age & city. 
+#  Then print a sentance that uses these variables.
+#Ans
+name = "Akash Joshi"
+age = 23
+city = "Thane"
+print("My name is", name,"from", city, "& I'm", age)
+# OR
+print(f"My name is {name} from {city} & I'm {age}")
