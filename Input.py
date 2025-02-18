4aeb614a0c14d3fa48e3ad9da1e57a4e9104e144
Akash Joshi <117718217+AkashJoshi9@users.noreply.github.com>
2025-02-19 01:33:10 +0530

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,14 @@
+# input funtion for python 
+
+# input reads as a string and returns it. 
+
+# name = input("Enter your name:")
+# print(f"Welcome {name} to python.")
+
+# age = input("Enter your age:")
+# print(f"Great! So 10 years after you will be a {int(age)+10}!")
+
+# input from user to add two numbers and print result
+a = input("Enter first number:")
+b = input("Enter second number:")
+print(f"The Answer is {int(a)+int(b)}")
