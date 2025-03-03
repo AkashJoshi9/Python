1f966d7f622fd7f762501b76707591a98fa02e3d
Akash Joshi <117718217+AkashJoshi9@users.noreply.github.com>
2025-03-04 00:39:58 +0530

    Update Input.py

--- 5- Input Function/Input.py
+++ 5- Input Function/Input.py
@@ -12,3 +12,35 @@
 a = float(input("Enter first number:"))
 b = float(input("Enter second number:"))
 print(f"The Answer is {a+b}")
+
+# input from user to basic calculator and print result
+print("Choose Option - 'add, mul, div, subs, avg, exit'")
+
+while True:
+    user = input("Choose the operation:")
+    if user == 'exit':
+      print("You are exit!")
+      break
+
+    a = float(input("Enter first number:"))
+    b = float(input("Enter second number:"))
+    if user == 'add':
+      result = a + b 
+      print(f"Answer: {result}")
+    elif user == 'mul':
+      result = a * b 
+      print(f"Answer: {result}")
+    elif user == 'subs':
+      result = a - b
+      print(f"Answer: {result}")
+    elif user == 'div':
+     if b != 0:
+        result = a / b
+        print(f"The result is: {result}")
+     else:
+        print("Error! Cannot divide by zero.")
+    elif user == 'avg':
+      result = (a + b)/2
+      print(f"Answer: {result}")
+    else:
+      print("error!")
